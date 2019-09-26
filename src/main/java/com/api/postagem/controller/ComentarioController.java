f4521fabe0ea21bcf8a26d90406bd1dabef12cb5
marlo2222 <marlo_henrique@hotmail.com.br>
2019-09-26 13:35:35 -0300

    atualizando o swagger

--- src/main/java/com/api/postagem/controller/ComentarioController.java
+++ src/main/java/com/api/postagem/controller/ComentarioController.java
@@ -5,6 +5,8 @@ import com.api.postagem.models.Postagem;
 import com.api.postagem.repository.ComentarioRepository;
 import com.api.postagem.repository.PostagemRepository;
 import com.api.postagem.services.ComentarioServices;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api")
+@Api(value = "API de favoritos")
 public class ComentarioController {
 
     @Autowired
@@ -22,16 +25,19 @@ public class ComentarioController {
     private ComentarioServices comentarioServices;
 
     @GetMapping(value = "/comentario/listar")
+    @ApiOperation(value = "retorna uma lista de comentarios")
     public ResponseEntity<?> listarComentarios(){
         return new ResponseEntity<>(comentarioRepository.findAll(), HttpStatus.OK);
     }
 
     @GetMapping(value = "/comentario/listar/{id}")
+    @ApiOperation(value = "retorna um comentario")
     public ResponseEntity<?> listarComentarioId(@PathVariable("id") long id){
         return new ResponseEntity<>(comentarioRepository.findById(id), HttpStatus.OK);
     }
 
     @PostMapping(value = "/comentario/salvar")
+    @ApiOperation(value = "salva um comentario")
     public ResponseEntity<?> salvarComentario(@RequestBody Comentario comentario){
         Postagem postagem = new Postagem();
         postagem = postagemRepository.findById(comentario.getPostagem().getId());
@@ -41,12 +47,14 @@ public class ComentarioController {
     }
 
     @DeleteMapping(value = "/comentario/deletar/{id}")
+    @ApiOperation(value = "deleta um comentario")
     public ResponseEntity<?> deletarComentario(@PathVariable("id") long id){
         comentarioRepository.deleteById(id);
         return new ResponseEntity<>("deletado  com sucesso", HttpStatus.OK);
     }
 
     @PutMapping(value = "/comentario/atualizar")
+    @ApiOperation(value = "atualiza um comentario")
     public ResponseEntity<?> atualizarComentario(@RequestBody Comentario comentario){
         comentarioRepository.save(comentarioServices.atualizarData(comentario));
         return  new ResponseEntity<>("atualizado com sucesso", HttpStatus.OK);
--- src/main/java/com/api/postagem/controller/PostagemController.java
+++ src/main/java/com/api/postagem/controller/PostagemController.java
@@ -3,6 +3,8 @@ package com.api.postagem.controller;
 import com.api.postagem.models.Postagem;
 import com.api.postagem.repository.PostagemRepository;
 import com.api.postagem.services.PostagemServices;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@Api(value = "API de favoritos")
 public class PostagemController {
 
     @Autowired
@@ -19,29 +22,34 @@ public class PostagemController {
     PostagemServices postagemServices;
 
 
-    @GetMapping(value = "/postagem/listar")
+    @GetMapping(value = "/postagem")
+    @ApiOperation(value = "retorna uma lista de postagem")
     public ResponseEntity<?> listarPostagens(){
         return new ResponseEntity<>(postagemRepository.findAll(), HttpStatus.OK);
     }
 
-    @GetMapping(value = "/postagem/listar/{id}")
+    @GetMapping(value = "/postagem/{id}")
+    @ApiOperation(value = "retorna uma postagem pelo id")
     public ResponseEntity<?> listarPostagemId(@PathVariable("id") long id){
         return new ResponseEntity<>(postagemRepository.findById(id),HttpStatus.OK);
     }
 
-    @PostMapping(value = "/postagem/salvar")
+    @PostMapping(value = "/postagem")
+    @ApiOperation(value = "salva uma postagem")
     public ResponseEntity<?> salvarPostagem(@RequestBody Postagem postagem){
         postagemRepository.save(postagemServices.definirData(postagem));
         return new ResponseEntity<>("Salvo com sucesso", HttpStatus.OK);
     }
 
-    @DeleteMapping(value = "/postagem/deletar/{id}")
+    @DeleteMapping(value = "/postagem/{id}")
+    @ApiOperation(value = "delete uma postagem")
     public ResponseEntity<?> deletarPostagem(@PathVariable("id") long id){
         postagemRepository.deleteById(id);
         return new ResponseEntity<>("Removido com sucesso", HttpStatus.OK);
     }
 
-    @PutMapping(value = "/postagem/atualizar")
+    @PutMapping(value = "/postagem")
+    @ApiOperation(value = "atualiza uma postagem")
     public ResponseEntity<?> atualizarPostagem(@RequestBody Postagem postagem){
         postagemRepository.save(postagemServices.atualizarData(postagem));
         return new ResponseEntity<>("Atualizado com sucesso", HttpStatus.OK);
