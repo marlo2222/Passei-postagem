40f83547b11a02d3da32f6d93d23b5901b481a98
marlo2222 <marlo_henrique@hotmail.com.br>
2023-04-22 17:08:34 -0300

    ci: adicionando performance test

--- src/main/java/com/api/postagem/PostagemApplication.java
+++ src/main/java/com/api/postagem/PostagemApplication.java
@@ -2,11 +2,8 @@ package com.api.postagem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class PostagemApplication {
 
 	public static void main(String[] args) {
