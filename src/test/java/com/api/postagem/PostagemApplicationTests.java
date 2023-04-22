60649ca0ed75cfe5bd2ac13c9d7700d32ec8bdd6
marlo2222 <marlo_henrique@hotmail.com.br>
2023-04-22 15:56:42 -0300

    ci: adicionando test unit ao actions

--- src/test/java/com/api/postagem/PostagemApplicationTests.java
+++ src/test/java/com/api/postagem/PostagemApplicationTests.java
@@ -1,16 +1,44 @@
 package com.api.postagem;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Date;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.api.postagem.models.Comentario;
+import com.api.postagem.models.Postagem;
+import com.api.postagem.repository.PostagemRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PostagemApplicationTests {
+	
+	@Autowired
+    PostagemRepository postagemRepository;
 
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+    public void testFindById() {
+        Postagem postagem = new Postagem();
+        postagem.setId(1L);
+        postagem.setTitulo("Título da postagem");
+        postagem.setDescicao("Descrição da postagem");
+        
+        postagemRepository.save(postagem);
+        Postagem postagemEncontrada = postagemRepository.findById(postagem.getId());
+        
+        assertNotNull(postagemEncontrada);
+        assertEquals(postagem.getId(), postagemEncontrada.getId());
+        assertEquals(postagem.getTitulo(), postagemEncontrada.getTitulo());
+        assertEquals(postagem.getDescicao(), postagemEncontrada.getDescicao());
+    };
 }
